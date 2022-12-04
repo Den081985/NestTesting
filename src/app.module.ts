5306981a302001739ea1570935e2867a3ec51474
Den081985 <pishnenkod@gmail.com>
2022-12-04 16:56:49 +0300

    Add auto and db modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AutoModule } from './auto/auto.module';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    AutoModule,
+    DbModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
